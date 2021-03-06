ff53f1f69deec0c2ee8935375d370c6fbcbab49e
Kenji Miyake <kenji.miyake@tier4.jp>
2021-03-06 22:41:57 +0900

    Fix bug
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

--- functions/colcon_abbr_register.fish
+++ functions/colcon_abbr_register.fish
@@ -10,17 +10,17 @@ function colcon_abbr_register
     set -l cb "colcon build$symlink$additional_options --cmake-args"
 
     abbr -a cb "$cb"
-    abbr -a cbd "$cb $debug$COLCON_ABBR_CMAKE_ARGS"
-    abbr -a cbr "$cb $release$COLCON_ABBR_CMAKE_ARGS"
-    abbr -a cbrd "$cb $relwithdebinfo$COLCON_ABBR_CMAKE_ARGS"
+    abbr -a cbd "$cb $debug$cmake_args"
+    abbr -a cbr "$cb $release$cmake_args"
+    abbr -a cbrd "$cb $relwithdebinfo$cmake_args"
 
     abbr -a cbp "$cb --packages-up-to"
-    abbr -a cbdp "$cb $debug$COLCON_ABBR_CMAKE_ARGS --packages-up-to"
-    abbr -a cbrp "$cb $release$COLCON_ABBR_CMAKE_ARGS --packages-up-to"
-    abbr -a cbrdp "$cb $relwithdebinfo$COLCON_ABBR_CMAKE_ARGS --packages-up-to"
+    abbr -a cbdp "$cb $debug$cmake_args --packages-up-to"
+    abbr -a cbrp "$cb $release$cmake_args --packages-up-to"
+    abbr -a cbrdp "$cb $relwithdebinfo$cmake_args --packages-up-to"
 
     abbr -a cbps "$cb --packages-select"
-    abbr -a cbdps "$cb $debug$COLCON_ABBR_CMAKE_ARGS --packages-select"
-    abbr -a cbrps "$cb $release$COLCON_ABBR_CMAKE_ARGS --packages-select"
-    abbr -a cbrdps "$cb $relwithdebinfo$COLCON_ABBR_CMAKE_ARGS --packages-select"
+    abbr -a cbdps "$cb $debug$cmake_args --packages-select"
+    abbr -a cbrps "$cb $release$cmake_args --packages-select"
+    abbr -a cbrdps "$cb $relwithdebinfo$cmake_args --packages-select"
 end
