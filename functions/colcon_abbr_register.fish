b68b608e9724ff12001a6009d32a33dd879aa2e9
Kenji Miyake <31987104+kenji-miyake@users.noreply.github.com>
2021-11-02 01:39:45 +0900

    Refactor (#2)
    * Add comments
    
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>
    
    * Rename variable
    
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>
    
    * Simplify code
    
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>
    
    * Split to a function
    
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

--- functions/colcon_abbr_register.fish
+++ functions/colcon_abbr_register.fish
@@ -1,26 +1,30 @@
 function colcon_abbr_register
+    register_colcon_build
+end
+
+function register_colcon_build
     $COLCON_ABBR_USE_SYMLINK && set symlink " --symlink-install" || set symlink ""
     set -q COLCON_ABBR_ADDITIONAL_OPTIONS && set additional_options " $COLCON_ABBR_ADDITIONAL_OPTIONS" || set additional_options ""
     set -q COLCON_ABBR_CMAKE_ARGS && set cmake_args " $COLCON_ABBR_CMAKE_ARGS" || set cmake_args ""
 
     set -l debug "-DCMAKE_BUILD_TYPE=Debug"
     set -l release "-DCMAKE_BUILD_TYPE=Release"
-    set -l relwithdebinfo "-DCMAKE_BUILD_TYPE=RelWithDebInfo"
+    set -l rel_with_deb_info "-DCMAKE_BUILD_TYPE=RelWithDebInfo"
 
-    set -l cb "colcon build$symlink$additional_options --cmake-args"
+    set -l cb "colcon build$symlink$additional_options --cmake-args$cmake_args"
 
-    abbr -a cb "$cb"
-    abbr -a cbd "$cb $debug$cmake_args"
-    abbr -a cbr "$cb $release$cmake_args"
-    abbr -a cbrd "$cb $relwithdebinfo$cmake_args"
+    # colcon build
+    abbr -a cbd "$cb $debug"
+    abbr -a cbr "$cb $release"
+    abbr -a cbrd "$cb $rel_with_deb_info"
 
-    abbr -a cbp "$cb --packages-up-to"
-    abbr -a cbdp "$cb $debug$cmake_args --packages-up-to"
-    abbr -a cbrp "$cb $release$cmake_args --packages-up-to"
-    abbr -a cbrdp "$cb $relwithdebinfo$cmake_args --packages-up-to"
+    # colcon build --packages-up-to
+    abbr -a cbdp "$cb $debug --packages-up-to"
+    abbr -a cbrp "$cb $release --packages-up-to"
+    abbr -a cbrdp "$cb $rel_with_deb_info --packages-up-to"
 
-    abbr -a cbps "$cb --packages-select"
-    abbr -a cbdps "$cb $debug$cmake_args --packages-select"
-    abbr -a cbrps "$cb $release$cmake_args --packages-select"
-    abbr -a cbrdps "$cb $relwithdebinfo$cmake_args --packages-select"
+    # colcon build --packages-select
+    abbr -a cbdps "$cb $debug --packages-select"
+    abbr -a cbrps "$cb $release --packages-select"
+    abbr -a cbrdps "$cb $rel_with_deb_info --packages-select"
 end
